d8cc6bdba43d91e8fd8bd00f417b24fb6a305b46
nhhanh06122k3 <anhnhhhe170445@fpt.edu.vn>
2025-02-28 00:06:11 +0700

    First commit

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// filepath: /src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddProduct from './dashboard/view/seller/AddProduct';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/add-product" component={AddProduct} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
