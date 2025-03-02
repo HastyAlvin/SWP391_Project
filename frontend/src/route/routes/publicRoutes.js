1d20dc1b26e642d89bd973b547fd9c44ad68efb1
Hasty Alvin <quangthang0311@gmail.com>
2025-03-02 23:39:23 +0700

    add Router component for managing routes

---
+++ frontend/src/route/routes/publicRoutes.js
@@ -0,0 +1,14 @@
+import { lazy } from "react";    
+const AdminLogin = lazy(()=> import('../../views/auth/AdminLogin')) 
+
+
+const publicRoutes = [
+   
+    {
+        path : '/admin/login',
+        element : <AdminLogin/>
+    },
+   
+]
+
+export default publicRoutes
