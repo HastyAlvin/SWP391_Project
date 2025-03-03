2ad13c54becfccbc0df9d719db1dd29fa1478906
Hasty Alvin <quangthang0311@gmail.com>
2025-03-04 01:10:01 +0700

    add admin login controller

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,48 @@
+const adminModel = require('../models/adminModel')
+const sellerModel = require('../models/sellerModel')
+const sellerCustomerModel  = require('../models/chat/sellerCustomerModel')
+const { responseReturn } = require('../utiles/response')
+const bcrypt = require('bcrypt')
+const { createToken } = require('../utiles/tokenCreate')
+const cloudinary = require('cloudinary').v2
+const formidable = require("formidable")
+
+class authControllers{
+   
+    admin_login = async(req,res) => {
+        const {email,password} = req.body
+        try {
+            const admin = await adminModel.findOne({email}).select('+password')
+            // console.log(admin)
+            if (admin) {
+                const match = await bcrpty.compare(password, admin.password)
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : admin.id,
+                        role : admin.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires : new Date(Date.now() + 7*24*60*60*1000 )
+                    }) 
+                    responseReturn(res,200,{token,message: "Login Success"})
+                } else {
+                    responseReturn(res,404,{error: "Password Wrong"})
+                }
+                 
+            } else {
+                responseReturn(res,404,{error: "Email not Found"})
+            }
+            
+        } catch (error) {
+            responseReturn(res,500,{error: error.message})
+        }
+ 
+    }
+    // End Method 
+
+
+  
+}
+
+module.exports = new authControllers()
