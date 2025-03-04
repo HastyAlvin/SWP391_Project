296a84c72841b8f2546cfbfd2dde43c9bbbd9aaf
Hasty Alvin <quangthang0311@gmail.com>
2025-03-04 23:59:36 +0700

    Add Router

--- frontend/src/router/routes/privateRoutes.js
+++ frontend/src/router/routes/privateRoutes.js
@@ -1,5 +1,7 @@
 import { adminRoutes } from "./adminRoutes";
+import  { sellerRoutes } from './sellerRoutes';
 
 export const privateRoutes = [
     ...adminRoutes,
+    ...sellerRoutes
 ]
--- frontend/src/router/routes/sellerRoutes.js
+++ frontend/src/router/routes/sellerRoutes.js
@@ -1,17 +1,19 @@
 import { lazy } from "react";            
 
-const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))   
-const Products = lazy(()=> import('../../views/seller/Products')) 
-const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts')) 
-const Orders = lazy(()=> import('../../views/seller/Orders')) 
-const Payments = lazy(()=> import('../../views/seller/Payments'))
-const SellerToAdmin = lazy(()=> import('../../views/seller/SellerToAdmin'))
-const SellerToCustomer = lazy(()=> import('../../views/seller/SellerToCustomer'))
-const Profile = lazy(()=> import('../../views/seller/Profile'))
-const EditProduct = lazy(()=> import('../../views/seller/EditProduct'))
-const OrderDetails = lazy(()=> import('../../views/seller/OrderDetails'))
+const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))   
+// const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))   
+// const Products = lazy(()=> import('../../views/seller/Products')) 
+// const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts')) 
+// const Orders = lazy(()=> import('../../views/seller/Orders')) 
+// const Payments = lazy(()=> import('../../views/seller/Payments'))
+// const SellerToAdmin = lazy(()=> import('../../views/seller/SellerToAdmin'))
+// const SellerToCustomer = lazy(()=> import('../../views/seller/SellerToCustomer'))
+// const Profile = lazy(()=> import('../../views/seller/Profile'))
+// const EditProduct = lazy(()=> import('../../views/seller/EditProduct'))
+// const OrderDetails = lazy(()=> import('../../views/seller/OrderDetails'))
 const Pending = lazy(()=> import('./../../views/Pending')) 
-const AddBanner = lazy(()=> import('../../views/seller/AddBanner')) 
+const Deactive = lazy(()=> import('./../../views/Deactive')) 
+// const AddBanner = lazy(()=> import('../../views/seller/AddBanner')) 
 
 export const sellerRoutes = [
     
@@ -20,78 +22,88 @@ export const sellerRoutes = [
         element : <Pending/>,
         ability : 'seller' 
     },
-    
     {
-        path: '/seller/dashboard/add-product',
-        element : <AddProduct/>,
+        path: '/seller/account-deactive',
+        element : <Deactive/>,
+        ability : 'seller' 
+    },
+    {
+        path: '/seller/dashboard',
+        element : <SellerDashboard/>,
         role : 'seller',
         status : 'active'
     },
-    {
-        path: '/seller/dashboard/edit-product/:productId',
-        element : <EditProduct/>,
-        role : 'seller',
-        status : 'active'
-    },
-    {
-        path: '/seller/dashboard/products',
-        element : <Products/>,
-        role : 'seller',
-        status : 'active'
-    },
-    {
-        path: '/seller/dashboard/discount-product',
-        element : <DiscountProducts/>,
-        role : 'seller',
-        status : 'active'
-    },
-    {
-        path: '/seller/dashboard/orders',
-        element : <Orders/>,
-        role : 'seller',
-        visibility : ['active','deactive']
-    },
-    {
-        path: '/seller/dashboard/order/details/:orderId',
-        element : <OrderDetails/>,
-        role : 'seller',
-        visibility : ['active','deactive']
-    },
-    {
-        path: '/seller/dashboard/payments',
-        element : <Payments/>,
-        role : 'seller',
-        status : 'active'
-    },
-    {
-        path: '/seller/dashboard/chat-support',
-        element : <SellerToAdmin/>,
-        role : 'seller',
-        visibility : ['active','deactive','pending']
-    },
-    {
-        path: '/seller/dashboard/chat-customer/:customerId',
-        element : <SellerToCustomer/>,
-        role : 'seller',
-        status : 'active'
-    },
-    { 
-        path: '/seller/dashboard/chat-customer',
-        element : <SellerToCustomer/>,
-        role : 'seller',
-        status : 'active'
-    },
-    {
-        path: '/seller/dashboard/profile',
-        element : <Profile/>,
-        role : 'seller',
-        visibility : ['active','deactive','pending']
-    },
-    {
-        path: '/seller/dashboard/add-banner/:productId',
-        element : <AddBanner/>,
-        role : 'seller',
-        status : 'active'
-    }
+    // {
+    //     path: '/seller/dashboard/add-product',
+    //     element : <AddProduct/>,
+    //     role : 'seller',
+    //     status : 'active'
+    // },
+    // {
+    //     path: '/seller/dashboard/edit-product/:productId',
+    //     element : <EditProduct/>,
+    //     role : 'seller',
+    //     status : 'active'
+    // },
+    // {
+    //     path: '/seller/dashboard/products',
+    //     element : <Products/>,
+    //     role : 'seller',
+    //     status : 'active'
+    // },
+    // {
+    //     path: '/seller/dashboard/discount-product',
+    //     element : <DiscountProducts/>,
+    //     role : 'seller',
+    //     status : 'active'
+    // },
+    // {
+    //     path: '/seller/dashboard/orders',
+    //     element : <Orders/>,
+    //     role : 'seller',
+    //     visibility : ['active','deactive']
+    // },
+    // {
+    //     path: '/seller/dashboard/order/details/:orderId',
+    //     element : <OrderDetails/>,
+    //     role : 'seller',
+    //     visibility : ['active','deactive']
+    // },
+    // {
+    //     path: '/seller/dashboard/payments',
+    //     element : <Payments/>,
+    //     role : 'seller',
+    //     status : 'active'
+    // },
+    // {
+    //     path: '/seller/dashboard/chat-support',
+    //     element : <SellerToAdmin/>,
+    //     role : 'seller',
+    //     visibility : ['active','deactive','pending']
+    // },
+    // {
+    //     path: '/seller/dashboard/chat-customer/:customerId',
+    //     element : <SellerToCustomer/>,
+    //     role : 'seller',
+    //     status : 'active'
+    // },
+    // { 
+    //     path: '/seller/dashboard/chat-customer',
+    //     element : <SellerToCustomer/>,
+    //     role : 'seller',
+    //     status : 'active'
+    // },
+    // {
+    //     path: '/seller/dashboard/profile',
+    //     element : <Profile/>,
+    //     role : 'seller',
+    //     visibility : ['active','deactive','pending']
+    // },
+    // {
+    //     path: '/seller/dashboard/add-banner/:productId',
+    //     element : <AddBanner/>,
+    //     role : 'seller',
+    //     status : 'active'
+    // }
 
 ]
