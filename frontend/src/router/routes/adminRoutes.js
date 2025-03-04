f398c1d68b68bd5c95f223150b773efbd7591bb5
Hasty Alvin <quangthang0311@gmail.com>
2025-03-04 20:58:03 +0700

    Update router FE

---
+++ frontend/src/router/routes/adminRoutes.js
@@ -0,0 +1,45 @@
+      
+export const adminRoutes = [
+    // {
+    //     path: 'admin/dashboard',
+    //     element : <AdminDashboard/>,
+    //     role : 'admin'
+    // },
+    // {
+    //     path: 'admin/dashboard/orders',
+    //     element : <Orders/> ,
+    //     role : 'admin'
+    // },
+    // {
+    //     path: 'admin/dashboard/category',
+    //     element : <Category/> ,
+    //     role : 'admin'
+    // },
+    // {
+    //     path: 'admin/dashboard/sellers',
+    //     element : <Sellers/> ,
+    //     role : 'admin'
+    // },
+    // {
+    //     path: 'admin/dashboard/deactive-sellers',
+    //     element : <DeactiveSellers/> ,
+    //     role : 'admin'
+    // },
+    // {
+    //     path: 'admin/dashboard/sellers-request',
+    //     element : <SellerRequest/> ,
+    //     role : 'admin'
+    // },
+    // {
+    //     path: 'admin/dashboard/seller/details/:sellerId',
+    //     element : <SellerDetails/> ,
+    //     role : 'admin'
+    // }, 
+
+    // {
+    //     path: 'admin/dashboard/order/details/:orderId',
+    //     element : <OrderDetails/> ,
+    //     role : 'admin'
+    // },
+ 
+]
