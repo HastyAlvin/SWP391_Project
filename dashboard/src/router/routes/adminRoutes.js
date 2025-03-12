0c74732de60d5b291b016488f58d95290cad5874
nhhanh06122k3 <anhnhhhe170445@fpt.edu.vn>
2025-03-12 15:46:54 +0700

    update card

---
+++ dashboard/src/router/routes/adminRoutes.js
@@ -0,0 +1,19 @@
+import { lazy } from "react";
+
+
+const AdminDashboard = lazy(() => import("../../views/admin/AdminDashboard"));
+const Orders = lazy(() => import("../../views/admin/Order"));
+
+export const adminRoutes = [
+  {
+    path: "admin/dashboard",
+    element: <AdminDashboard />,
+    role: "admin",
+  },
+
+  {
+    path: "admin/dashboard/orders",
+    element: <Orders />,
+    role: "admin",
+  },
+];
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Card from './pages/Card';
+import { get_category } from './store/reducers/homeReducer';
+import { useDispatch} from 'react-redux';
+
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(get_category()) 
+},[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/card' element={<Card/>} />
+    </Routes>
+    
+    </BrowserRouter>
   );
 }
 
