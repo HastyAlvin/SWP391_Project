b950cc78dbe355c35d09f448f66891ac4cdeff91
huyvthe130028 <huyvthe130028@fpt.edu.vn>
2025-03-27 13:49:37 +0700

    fix

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,211 +1,211 @@
-    const adminModel = require("../models/adminModel");
-    const sellerModel = require("../models/sellerModel");
-    const { responseReturn } = require("../utiles/response");
-    const bcrypt = require("bcrypt");
-    const { createToken } = require("../utiles/tokenCreate");
-    const cloudinary = require("cloudinary").v2;
-    const formidable = require("formidable");
+const adminModel = require("../models/adminModel");
+const sellerModel = require("../models/sellerModel");
+const { responseReturn } = require("../utiles/response");
+const bcrypt = require("bcrypt");
+const { createToken } = require("../utiles/tokenCreate");
+const cloudinary = require("cloudinary").v2;
+const formidable = require("formidable");
 
-    class authControllers {
-        admin_login = async (req, res) => {
-            const { email, password } = req.body;
-            try {
-                const admin = await adminModel.findOne({ email }).select("+password");
-                // console.log(admin)
-                if (admin) {
-                    const match = await bcrypt.compare(password, admin.password);
-                    // console.log(match)
-                    if (match) {
-                        const token = await createToken({
-                            id: admin.id,
-                            role: admin.role,
-                        });
-                        res.cookie("accessToken", token, {
-                            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                        });
-                        responseReturn(res, 200, { token, message: "Login Success" });
-                    } else {
-                        responseReturn(res, 404, { error: "Password Wrong" });
-                    }
-                } else {
-                    responseReturn(res, 404, { error: "Email not Found" });
-                }
-            } catch (error) {
-                responseReturn(res, 500, { error: error.message });
-            }
-        };
-        // End Method
-
-        seller_login = async (req, res) => {
-            const { email, password } = req.body;
-            try {
-                const seller = await sellerModel.findOne({ email }).select("+password");
-                // console.log(admin)
-                if (seller) {
-                    const match = await bcrypt.compare(password, seller.password);
-                    // console.log(match)
-                    if (match) {
-                        const token = await createToken({
-                            id: seller.id,
-                            role: seller.role,
-                        });
-                        res.cookie("accessToken", token, {
-                            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                        });
-                        responseReturn(res, 200, { token, message: "Login Success" });
-                    } else {
-                        responseReturn(res, 404, { error: "Password Wrong" });
-                    }
-                } else {
-                    responseReturn(res, 404, { error: "Email not Found" });
-                }
-            } catch (error) {
-                responseReturn(res, 500, { error: error.message });
-            }
-        };
-        // End Method
-
-        seller_register = async (req, res) => {
-            const { email, name, password } = req.body;
-            try {
-                const getUser = await sellerModel.findOne({ email });
-                if (getUser) {
-                    responseReturn(res, 404, { error: "Email Already Exit" });
-                } else {
-                    const seller = await sellerModel.create({
-                        name,
-                        email,
-                        password: await bcrypt.hash(password, 10),
-                        method: "menualy",
-                        shopInfo: {},
+class authControllers {
+    admin_login = async (req, res) => {
+        const { email, password } = req.body;
+        try {
+            const admin = await adminModel.findOne({ email }).select("+password");
+            // console.log(admin)
+            if (admin) {
+                const match = await bcrypt.compare(password, admin.password);
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id: admin.id,
+                        role: admin.role,
                     });
-
-                    const token = await createToken({ id: seller.id, role: seller.role });
                     res.cookie("accessToken", token, {
                         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
                     });
-
-                    responseReturn(res, 201, { token, message: "Register Success" });
-                }
-            } catch (error) {
-                responseReturn(res, 500, { error: "Internal Server Error" });
-            }
-        };
-        // End Method
-
-        getUser = async (req, res) => {
-            const { id, role } = req;
-
-            try {
-                if (role === "admin") {
-                    const user = await adminModel.findById(id);
-                    responseReturn(res, 200, { userInfo: user });
+                    responseReturn(res, 200, { token, message: "Login Success" });
                 } else {
-                    const seller = await sellerModel.findById(id);
-                    responseReturn(res, 200, { userInfo: seller });
+                    responseReturn(res, 404, { error: "Password Wrong" });
                 }
-            } catch (error) {
-                responseReturn(res, 500, { error: "Internal Server Error" });
+            } else {
+                responseReturn(res, 404, { error: "Email not Found" });
             }
-        }; // End getUser Method
+        } catch (error) {
+            responseReturn(res, 500, { error: error.message });
+        }
+    };
+    // End Method
 
-        profile_image_upload = async (req, res) => {
-            const { id } = req;
-            const form = formidable({ multiples: true });
-            form.parse(req, async (err, _, files) => {
-                cloudinary.config({
-                    cloud_name: process.env.cloud_name,
-                    api_key: process.env.api_key,
-                    api_secret: process.env.api_secret,
-                    secure: true,
-                });
-                const { image } = files;
-
-                try {
-                    const result = await cloudinary.uploader.upload(image.filepath, {
-                        folder: "profile",
+    seller_login = async (req, res) => {
+        const { email, password } = req.body;
+        try {
+            const seller = await sellerModel.findOne({ email }).select("+password");
+            // console.log(admin)
+            if (seller) {
+                const match = await bcrypt.compare(password, seller.password);
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id: seller.id,
+                        role: seller.role,
                     });
-                    if (result) {
-                        await sellerModel.findByIdAndUpdate(id, {
-                            image: result.url,
-                        });
-                        const userInfo = await sellerModel.findById(id);
-                        responseReturn(res, 201, {
-                            message: "Profile Image Upload Successfully",
-                            userInfo,
-                        });
-                    } else {
-                        responseReturn(res, 404, { error: "Image Upload Failed" });
-                    }
-                } catch (error) {
-                    responseReturn(res, 500, { error: error.message });
+                    res.cookie("accessToken", token, {
+                        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+                    });
+                    responseReturn(res, 200, { token, message: "Login Success" });
+                } else {
+                    responseReturn(res, 404, { error: "Password Wrong" });
                 }
+            } else {
+                responseReturn(res, 404, { error: "Email not Found" });
+            }
+        } catch (error) {
+            responseReturn(res, 500, { error: error.message });
+        }
+    };
+    // End Method
+
+    seller_register = async (req, res) => {
+        const { email, name, password } = req.body;
+        try {
+            const getUser = await sellerModel.findOne({ email });
+            if (getUser) {
+                responseReturn(res, 404, { error: "Email Already Exit" });
+            } else {
+                const seller = await sellerModel.create({
+                    name,
+                    email,
+                    password: await bcrypt.hash(password, 10),
+                    method: "menualy",
+                    shopInfo: {},
+                });
+
+                const token = await createToken({ id: seller.id, role: seller.role });
+                res.cookie("accessToken", token, {
+                    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+                });
+
+                responseReturn(res, 201, { token, message: "Register Success" });
+            }
+        } catch (error) {
+            responseReturn(res, 500, { error: "Internal Server Error" });
+        }
+    };
+    // End Method
+
+    getUser = async (req, res) => {
+        const { id, role } = req.user;
+
+        try {
+            if (role === "admin") {
+                const user = await adminModel.findById(id);
+                responseReturn(res, 200, { userInfo: user });
+            } else {
+                const seller = await sellerModel.findById(id);
+                responseReturn(res, 200, { userInfo: seller });
+            }
+        } catch (error) {
+            responseReturn(res, 500, { error: "Internal Server Error" });
+        }
+    }; // End getUser Method
+
+    profile_image_upload = async (req, res) => {
+        const { id } = req;
+        const form = formidable({ multiples: true });
+        form.parse(req, async (err, _, files) => {
+            cloudinary.config({
+                cloud_name: process.env.cloud_name,
+                api_key: process.env.api_key,
+                api_secret: process.env.api_secret,
+                secure: true,
             });
-        };
-
-        // End Method
-
-        profile_info_add = async (req, res) => {
-            const { division, district, shopName, sub_district } = req.body;
-            const { id } = req;
+            const { image } = files;
 
             try {
-                await sellerModel.findByIdAndUpdate(id, {
-                    shopInfo: {
-                        shopName,
-                        division,
-                        district,
-                        sub_district,
-                    },
+                const result = await cloudinary.uploader.upload(image.filepath, {
+                    folder: "profile",
                 });
-                const userInfo = await sellerModel.findById(id);
-                responseReturn(res, 201, {
-                    message: "Profile info Add Successfully",
-                    userInfo,
-                });
-            } catch (error) {
-                responseReturn(res, 500, { error: error.message });
-            }
-        };
-        // End Method
-        profile_info_update = async (req, res) => {   
-            const { division, district, shopName, sub_district } = req.body;
-            const { id } = req;
-        
-            try {
-                const updatedSeller = await sellerModel.findByIdAndUpdate(
-                    id,
-                    { shopInfo: { shopName, division, district, sub_district } },
-                    { new: true } 
-                );
-        
-                if (!updatedSeller) {
-                    return res.status(404).json({ error: "Seller not found" });
+                if (result) {
+                    await sellerModel.findByIdAndUpdate(id, {
+                        image: result.url,
+                    });
+                    const userInfo = await sellerModel.findById(id);
+                    responseReturn(res, 201, {
+                        message: "Profile Image Upload Successfully",
+                        userInfo,
+                    });
+                } else {
+                    responseReturn(res, 404, { error: "Image Upload Failed" });
                 }
-        
-                responseReturn(res, 200, {
-                    message: "Profile info updated successfully",
-                    userInfo: updatedSeller
-                });
             } catch (error) {
                 responseReturn(res, 500, { error: error.message });
             }
-        };
-        
-        
-        logout = async (req, res) => {
-            try {
-                res.cookie("accessToken", null, {
-                    expires: new Date(Date.now()),
-                    httpOnly: true,
-                });
-                responseReturn(res, 200, { message: "logout Success" });
-            } catch (error) {
-                responseReturn(res, 500, { error: error.message });
-            }
-        };
-        // End Method
-        
-    }
+        });
+    };
 
-    module.exports = new authControllers();
+    // End Method
+
+    profile_info_add = async (req, res) => {
+        const { division, district, shopName, sub_district } = req.body;
+        const { id } = req;
+
+        try {
+            await sellerModel.findByIdAndUpdate(id, {
+                shopInfo: {
+                    shopName,
+                    division,
+                    district,
+                    sub_district,
+                },
+            });
+            const userInfo = await sellerModel.findById(id);
+            responseReturn(res, 201, {
+                message: "Profile info Add Successfully",
+                userInfo,
+            });
+        } catch (error) {
+            responseReturn(res, 500, { error: error.message });
+        }
+    };
+    // End Method
+    profile_info_update = async (req, res) => {
+        const { division, district, shopName, sub_district } = req.body;
+        const { id } = req;
+
+        try {
+            const updatedSeller = await sellerModel.findByIdAndUpdate(
+                id,
+                { shopInfo: { shopName, division, district, sub_district } },
+                { new: true }
+            );
+
+            if (!updatedSeller) {
+                return res.status(404).json({ error: "Seller not found" });
+            }
+
+            responseReturn(res, 200, {
+                message: "Profile info updated successfully",
+                userInfo: updatedSeller
+            });
+        } catch (error) {
+            responseReturn(res, 500, { error: error.message });
+        }
+    };
+
+
+    logout = async (req, res) => {
+        try {
+            res.cookie("accessToken", null, {
+                expires: new Date(Date.now()),
+                httpOnly: true,
+            });
+            responseReturn(res, 200, { message: "logout Success" });
+        } catch (error) {
+            responseReturn(res, 500, { error: error.message });
+        }
+    };
+    // End Method
+
+}
+
+module.exports = new authControllers();
