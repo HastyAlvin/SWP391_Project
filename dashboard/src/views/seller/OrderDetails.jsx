e60fedf04e730de40313132ef4628543d3a0ea15
Thang Pham <quangthang0311@gmail.com>
2025-03-30 10:06:04 +0700

    Revert "Fix"
    This reverts commit b3386ee66c7543665918f0b7ca4512793c437bde.

--- dashboard/src/views/seller/OrderDetails.jsx
+++ dashboard/src/views/seller/OrderDetails.jsx
@@ -39,10 +39,10 @@ const OrderDetails = () => {
 
     return (
         <div className='px-2 lg:px-7 pt-5'>
-            <div className='w-full p-4 bg-[#FFFFFF] rounded-md'>
+            <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
                 <div className='flex justify-between items-center p-4'>
-                    <h2 className='text-xl text-[#000000]'>Order Details</h2>
-                    <select onChange={status_update} value={status} name="" id="" className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#FFFFFF] border border-slate-700 rounded-md text-[#000000]'>
+                    <h2 className='text-xl text-[#d0d2d6]'>Order Details</h2>
+                    <select onChange={status_update} value={status} name="" id="" className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#475569] border border-slate-700 rounded-md text-[#d0d2d6]'>
                         <option value="pending">pending</option>
                         <option value="processing">processing</option>
                         <option value="warehouse">warehouse</option>
@@ -52,14 +52,14 @@ const OrderDetails = () => {
                 </div>
 
                 <div className='p-4'>
-                    <div className='flex gap-2 text-lg text-[#000000]'>
+                    <div className='flex gap-2 text-lg text-[#d0d2d6]'>
                         <h2>#{order._id}</h2>
                         <span>{order.date}</span>
                     </div>
 
                     <div className='flex flex-wrap'>
                         <div className='w-[30%]'>
-                            <div className='pr-3 text-[#000000] text-lg'>
+                            <div className='pr-3 text-[#d0d2d6] text-lg'>
                                 <div className='flex flex-col gap-1'>
                                     <h2 className='pb-2 font-semibold'>Deliver To : {order.shippingInfo} </h2>
 
@@ -71,8 +71,8 @@ const OrderDetails = () => {
                                 <span>Price : ${order.price}</span>
 
                                 {
-                                    order?.products?.map((p, i) => <div key={i} className='mt-4 flex flex-col gap-4 bg-[#FFFFFF] rounded-md'>
-                                        <div className='text-[#000000]'>
+                                    order?.products?.map((p, i) => <div key={i} className='mt-4 flex flex-col gap-4 bg-[#8288ed] rounded-md'>
+                                        <div className='text-[#d0d2d6]'>
                                             <div className='flex gap-3 text-md'>
                                                 <img className='w-[50px] h-[50px]' src={p.images[0]} alt="" />
 
