86ae35aca0f2b95465b8f3560bf7d848d14d60fa
huyvthe130028 <huyvthe130028@fpt.edu.vn>
2025-04-10 20:59:35 +0700

    fix

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: 'http://localhost:5000/api',
-    headers: {
-        'Content-Type': 'multipart/form-data'
-    }
+    // headers: {
+    //     'Content-Type': 'multipart/form-data'
+    // }
 })
 
 export default api
