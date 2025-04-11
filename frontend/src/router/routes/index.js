861138e33d35eb20814e7b6106b0469d53d0798a
Thang Pham <quangthang0311@gmail.com>
2025-04-11 23:30:10 +0700

    Fix Router Front End

--- frontend/src/router/routes/index.js
+++ frontend/src/router/routes/index.js
@@ -1,16 +1,18 @@
-import MainLayout from "../../layout/MainLayout";
-import { privateRoutes } from "./privateRoutes";
-import ProtectRoute from "./ProtectRoute";
+import { privateRoutes } from './privateRoutes';
+import MainLayout from './../../layout/MainLayout';
+import ProtectRoute from './ProtectRoute';
+
 export const getRoutes = () => {
 
-  // eslint-disable-next-line array-callback-return
-  privateRoutes.map((route) => {
-    route.element = <ProtectRoute route={route}>{route.element}</ProtectRoute>;
-  });
+    privateRoutes.map(r => {
+        
+       r.element = <ProtectRoute route={r} >{r.element}</ProtectRoute>
+    })
 
-  return {
-    path: "/",
-    element: <MainLayout />,
-    children: privateRoutes,
-  };
-};
+
+    return {
+        path : '/',
+        element : <MainLayout />,
+        children : privateRoutes
+    }
+}
